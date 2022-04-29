e5099b4215541469d857d30c2d85fef97fc15736
Prince Kumar <Prince.kumar@incture.com>
2022-04-29 11:47:32 +0530

    added Proxy

--- src/example.js
+++ src/example.js
@@ -6,14 +6,6 @@ import {
   Link
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
 
 export default function BasicExample() {
   return (
@@ -32,14 +24,6 @@ export default function BasicExample() {
         </ul>
 
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
@@ -56,9 +40,6 @@ export default function BasicExample() {
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
-
 function Home() {
   return (
     <div>
@@ -70,7 +51,7 @@ function Home() {
 function About() {
   return (
     <div>
-      <h2>About...</h2>
+      <h2>About Site...</h2>
     </div>
   );
 }
